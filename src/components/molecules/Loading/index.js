053d2b576b5828f1ef4d672d797ef4f147f31fd1
Faridz Kurnia Chandra <faridzkurniaa@gmail.com>
2021-07-22 16:31:53 +0700

    Global Loading

---
+++ src/components/molecules/Loading/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
+
+const Loading = () => {
+    return (
+        <View style={styles.container}>
+            <ActivityIndicator size="large" color="#1ABC9C"/>
+            <Text style={styles.text}>Wait a sec...</Text>
+        </View>
+    )
+}
+
+export default Loading
+
+const styles = StyleSheet.create({
+    container: {
+        position:'absolute',
+        flex:1,
+        width:'100%',
+        height:'100%',
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    text: {
+        fontSize: 18,
+        fontFamily:'Poppins-Regular',
+        marginTop:12,
+    }
+})
