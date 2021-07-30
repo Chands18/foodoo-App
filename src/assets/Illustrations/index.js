18542e6232e90541b9ffad0418a8cdac3e4afb37
Faridz Kurnia Chandra <faridzkurniaa@gmail.com>
2021-07-30 15:09:23 +0700

    icon splash screen

--- src/assets/Illustrations/index.js
+++ src/assets/Illustrations/index.js
@@ -1,4 +1,4 @@
-import Logosplash from './Logosplash.svg';
+import Logosplash from './infinite1.svg';
 import IlSuccess from './Il-Success.svg'
 import IlSucorder from './successorder.svg'
 import IlEmpty from './emptyorder.svg'
